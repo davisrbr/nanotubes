d734888eb4e4ad1e1d023cff277cdb6bb5c25495
davisrbr <davisbr@live.unc.edu>
2020-04-15 12:22:19 -0400

    code speedup with single mesh

--- helper.py
+++ helper.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import streamlit as st
 from functools import reduce
 from numpy import errstate,isneginf,array
-import pandas as pd
 from pathlib import Path
+from typing import Dict, Callable, Tuple
 
 
 # global a0 length
@@ -20,135 +20,105 @@ def chiralAngle(chiralIndices : NamedTuple) -> float:
     return np.arctan(np.sqrt(3)*chiralIndices.m /
                      (2*chiralIndices.n + chiralIndices.m))
 
-def diameter(chiralIndices : NamedTuple, scale : float) -> float:
-    return BASIS_A0*scale/np.pi * np.sqrt(chiralIndices.n**2
+def diameter(chiralIndices : NamedTuple) -> float:
+    return BASIS_A0/np.pi * np.sqrt(chiralIndices.n**2
                                     + chiralIndices.m**2
                                     + chiralIndices.n*chiralIndices.m)
 
-def spacingD1(angle : float, scale : float) -> float:
+def spacingD1(angle : float) -> float:
     '''spacing for l1 pattern from R=0'''
-    return BASIS_A0*scale*np.cos(angle)
+    return BASIS_A0*np.cos(angle)
 
-def spacingD2(angle : float, scale : float) -> float:
+def spacingD2(angle : float) -> float:
     '''spacing for l2 pattern from R=0'''
-    return BASIS_A0*scale*np.cos((60*np.pi/180) - angle)
+    return BASIS_A0*np.cos((60*np.pi/180) - angle)
 
-def spacingD3(angle : float, scale : float) -> float:
+def spacingD3(angle : float) -> float:
     '''spacing for l3 pattern from R=0'''
-    return BASIS_A0*scale*np.cos((60*np.pi/180) + angle)
+    return BASIS_A0*np.cos((60*np.pi/180) + angle)
 
-def spacingD4(angle : float, scale : float) -> float:
+def spacingD4(angle : float) -> float:
     '''spacing for l4 pattern from R=0'''
-    return np.sqrt(3)*BASIS_A0*scale*np.cos((30*np.pi/180) - angle)
+    return np.sqrt(3)*BASIS_A0*np.cos((30*np.pi/180) - angle)
 
-def l0(chiralIndices : NamedTuple, X : np.ndarray,
-      Y : np.ndarray) -> np.ndarray:
+def l0_mesh(chiralIndices : NamedTuple, radius_spacing : np.ndarray) -> np.ndarray:
     '''2D mesh of values for l0'''
-    return np.abs(jv(0, X))**2 * Y
+    return np.abs(jv(0, radius_spacing))**2 
 
-def l1(chiralIndices : NamedTuple, X : np.ndarray,
-      Y : np.ndarray) -> np.ndarray:
+def l1_mesh(chiralIndices : NamedTuple, radius_spacing : np.ndarray) -> np.ndarray:
     '''2D mesh of values for l1'''
-    return np.abs(jv(chiralIndices.m, X))**2 * Y
+    return np.abs(jv(chiralIndices.m, radius_spacing))**2 
 
-def l2(chiralIndices : NamedTuple, X : np.ndarray,
-       Y : np.ndarray) -> np.ndarray:
+def l2_mesh(chiralIndices : NamedTuple, radius_spacing : np.ndarray) -> np.ndarray:
     '''2D mesh of values for l2'''
-    return np.abs(jv(chiralIndices.n, X))**2 * Y
+    return np.abs(jv(chiralIndices.n, radius_spacing))**2
 
-def l3(chiralIndices : NamedTuple, X : np.ndarray,
-       Y : np.ndarray) -> np.ndarray:
+def l3_mesh(chiralIndices : NamedTuple, radius_spacing : np.ndarray) -> np.ndarray:
     '''2D mesh of values for l3'''
-    return np.abs(jv(chiralIndices.n + chiralIndices.m, X))**2 * Y
+    return np.abs(jv(chiralIndices.n + chiralIndices.m, radius_spacing))**2 
 
-def l4(chiralIndices : NamedTuple, X : np.ndarray, 
-       Y : np.ndarray) -> np.ndarray:
+def l4_mesh(chiralIndices : NamedTuple, radius_spacing : np.ndarray) -> np.ndarray:
     '''2D mesh of values for l4'''
-    return np.abs(jv(chiralIndices.n - chiralIndices.m,X))**2 * Y
+    return np.abs(jv(chiralIndices.n - chiralIndices.m, radius_spacing))**2 
 
 @st.cache()
-def diffract_plot(chiral_n : int, chiral_m : int, 
+def diffract_plot(chiral_n : int, chiral_m : int, num_layer_lines : int, 
     scale : float, option : str) -> np.ndarray:
 
     # define indices from user input
     indices = chiralIndices(chiral_n  , chiral_m)
 
     # diameter of carbon nanotube
-    d       = diameter(indices, scale) 
+    d       = diameter(indices) 
 
     # chiral angle of carbon nanotube
     angle   = chiralAngle(indices) 
 
-    # D_i spacing of principal layer line i
-    D3      = spacingD3(angle, scale)
-    D2      = spacingD2(angle, scale)
-    D1      = spacingD1(angle, scale)
-
-    diffraction_distance = scale*BASIS_A0 # add space to layer lines
-
-    R = np.linspace(-d, d, 1000) #nm
-    radius_spacing = 2*np.pi*R
-    diffraction_spacing = np.linspace(-diffraction_distance, 
-                                    diffraction_distance, 600)
-
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-
-    #l0 centered
-    D0spacing = slice(298, 302)
-    y_plot[D0spacing] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l0_mesh = l0(indices, X, Y)
+    diameter_mesh = np.linspace(-d, d, 1000) #nm
+    radius_spacing = np.pi*diameter_mesh*scale
+    # diffraction_spacing = np.linspace(-diffraction_distance, 
+    #                                 diffraction_distance, 1000)
+    diffraction_spacing = radius_spacing.copy()
 
 
-    #l1 and D1
-    D1spacing_pos = slice(int(300*D1/diffraction_distance+300)-2, 
-                        int(300*D1/diffraction_distance+300)+2)
-    y_plot[D1spacing_pos] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l1_mesh_upper = l1(indices, X, Y)
+    # diffraction_distance =  scale*BASIS_A0 #np.pi*d*scale
+    # diffraction_distance = (radius_spacing[1]-radius_spacing[0])
+    diffraction_distance = np.pi*d*scale/70
 
-    D1spacing_neg = slice(-int(300*D1/diffraction_distance-300)-2, 
-                        -int(300*D1/diffraction_distance-300)+2)
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-    y_plot[D1spacing_neg] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l1_mesh_lower = l1(indices, X, Y)
 
-    #l2 and D2
-    D2spacing_pos = slice(int(300*D2/diffraction_distance+300)-2, 
-                        int(300*D2/diffraction_distance+300)+2)
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-    y_plot[D2spacing_pos] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l2_mesh_upper = l2(indices, X, Y)
 
-    D2spacing_neg = slice(-int(300*D2/diffraction_distance-300)-2, 
-                        -int(300*D2/diffraction_distance-300)+2)
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-    y_plot[D2spacing_neg] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l2_mesh_lower = l2(indices, X, Y)
+    mesh_layers : Dict[int, Tuple[Callable, float]] = {
+                                        1 : (l1_mesh, spacingD1(angle)),
+                                        2 : (l2_mesh, spacingD2(angle)),
+                                        3 : (l3_mesh, spacingD3(angle)),
+                                        4 : (l4_mesh, spacingD4(angle)) }
 
-    #l3 and D3
-    D3spacing_pos = slice(int(300*D3/diffraction_distance+300)-2, 
-                        int(300*D3/diffraction_distance+300)+2)
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-    y_plot[D3spacing_pos] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l3_mesh_upper = l3(indices, X, Y)
+    total_mesh = np.zeros((1000, 1000))
+    
+    for i in range(1, num_layer_lines+1):
+        
+        # Tuple[mesh_function, spacing in nm]
+        layer_line_func, position = mesh_layers[i] 
+            
+        # assumed layer line spacing based on 600 grid spaced Y-axis
+        pos_position_slice = int(np.floor(500*position/diffraction_distance+500))
+        neg_position_slice = -int(np.floor(500*position/diffraction_distance-500))
+        print(pos_position_slice)
 
-    D3spacing_neg = slice(-int(300*D3/diffraction_distance-300)-2, 
-                        -int(300*D3/diffraction_distance-300)+2)
-    y_plot = np.full_like(diffraction_spacing, 0, dtype=np.double)
-    y_plot[D3spacing_neg] = 1
-    (X,Y) = np.meshgrid(radius_spacing,y_plot)
-    l3_mesh_lower = l3(indices, X, Y)
+        # if not, scale is too 'zoomed in' to appear on plot, do nothing to mesh
+        if pos_position_slice+2 <=1000:
+            # define layer n_th line
+            layer_line = layer_line_func(indices, radius_spacing)
 
-    total_mesh = reduce(np.add, 
-                        (l0_mesh+5,
-                        l1_mesh_upper+5,l1_mesh_lower+5,
-                        l2_mesh_upper+5,l2_mesh_lower+5,
-                        l3_mesh_upper+5, l3_mesh_lower+5))
+            #define upper slice
+            diff_spacing_pos = slice(pos_position_slice-3, 
+                                     pos_position_slice+3)        
+
+            #define lower slice
+            diff_spacing_neg = slice(neg_position_slice-3, 
+                                  neg_position_slice+3)
+
+            total_mesh[diff_spacing_pos, :] = total_mesh[diff_spacing_neg, :] = layer_line
 
     # if logarithmic
     if option == 'Logarithmic':
@@ -156,8 +126,7 @@ def diffract_plot(chiral_n : int, chiral_m : int,
             total_mesh = np.log10(total_mesh)
             total_mesh[isneginf(total_mesh)]=0
 
-    X,Y = np.meshgrid(radius_spacing, diffraction_spacing)
-    return X, Y, total_mesh
+    return radius_spacing, diffraction_spacing, total_mesh 
 
 @st.cache()
 def read_markdown_file(markdown_file):
--- streamDiffractionDraft.py
+++ streamDiffractionDraft.py
@@ -6,8 +6,6 @@ import streamlit as st
 from functools import reduce
 from numpy import errstate,isneginf,array
 from helper import diffract_plot, chiralIndices, chiralAngle, diameter, read_markdown_file
-import altair as alt
-import pandas as pd
 
 # set page title
 st.title('Electron diffraction of carbon nanotube given chiral indices')
@@ -20,10 +18,10 @@ st.markdown("---")
 
 st.sidebar.markdown("## Plot Details")
 st.sidebar.markdown("① ** Diffraction intensity **")
-option = st.sidebar.selectbox('Logarithmic or linear diffraction intensity?', ('Logarithmic', 'Linear'))
+option = st.sidebar.selectbox('Logarithmic or linear diffraction intensity?', ('Linear', 'Logarithmic'))
 st.sidebar.markdown("---")
 st.sidebar.markdown("② ** Size scale of plot **")
-scale = st.sidebar.slider("Scale as fraction of 0.246 nm", 0.1, 10.0, 1.0, 0.1)
+scale = st.sidebar.slider("Scale as fraction of 0.246 nm", 0.1, 50.0, 10.0, 0.1)
 
 
 chiral_n = st.number_input("Chiral indice n", 0, 35, 17, 1)
@@ -32,16 +30,14 @@ indices  = chiralIndices(chiral_n, chiral_m)
 
 
 with st.spinner(text='Plotting new chiral indices'):
-    radius_spacing, diffraction_spacing, total_mesh = diffract_plot(indices.n, indices.m, scale, option)
-    plt.xticks([])
-    plt.yticks([])
-    plt.title(f'Chiral indices: [{indices.n},{indices.m}]      Diameter: {round(diameter(indices, scale), 3)}   Helicity: {round(chiralAngle(indices)*180/np.pi, 3)} degrees')
+                                                                                        #num_layer_lines here
+    radius_spacing, diffraction_spacing, total_mesh = diffract_plot(indices.n, indices.m, 3, scale, option)
+    # plt.xticks([])
+    # plt.yticks([])
+    plt.title(f'Chiral indices: [{indices.n},{indices.m}]      Diameter: {round(diameter(indices), 3)}   Helicity: {round(chiralAngle(indices)*180/np.pi, 3)} degrees')
     plt.pcolormesh(radius_spacing, diffraction_spacing, total_mesh,cmap='Blues')
     plt.show()
     st.pyplot()
 
 # scale = st.slider("Scale as fraction of 0.246 nm", 0.1, 10.0, 1.0, 0.1)
 # st.slider(l"Scale", min_value=None, max_value=None, value=None, step=None, format=None, key=None)
-
-# st.write(D1/diffraction_distance, D2/diffraction_distance,D3/diffraction_distance)
-# st.write(D1, D2, D3)
